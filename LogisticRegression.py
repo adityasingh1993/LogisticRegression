5909f9933597a2329918377ae8878a6f9ff94004
Aditya Kumar <aditya.kumar@aot.edu.in>
2017-08-21 00:15:12 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset=pd.read_csv('F:/UdemyML/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/Social_Network_Ads.csv')
+dataset=pd.read_csv('Social_Network_Ads.csv')
 X=dataset.iloc[:,[2,3]].values
 y=dataset.iloc[:,4].values
 
